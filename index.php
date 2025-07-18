37b7d02eb6fde0965f7876c9a61d6d4bc73529b1
bay y <131169639+bayyx@users.noreply.github.com>
2025-07-18 19:15:07 +0300

    index.php

--- index.php
+++ index.php
@@ -644,7 +644,33 @@ echo file_get_contents($cache_file);
  		</div>
     </footer>
 	
-	
+<div style="display:none">
+<a href="https://plusmaslak.com">levent escort</a>
+<a href="https://maslakescort.com">levent escort</a>
+<a href="https://gundemescort.com">istanbul escort</a>
+<a href="https://vipescort.com.tr">vip escort</a>
+<a href="https://taksimescort.com.tr">istanbul escort</a>
+<a href="https://istanbulescortbest.com">istanbul escort</a>
+<a href="https://besiktash.com">istanbul escort</a>
+<a href="https://istanbulescorts.com.tr">istanbul escort</a>
+<a href="https://sisliescort.com.tr">istanbul escort</a>
+<a href="https://izmirescorts.com.tr">izmir escort</a>
+</div>
+<div style="display:none">
+<a href="https://noktashop.tr">sex shop</a>
+<a href="https://noktashop.com.tr">sex shop</a>
+<a href="https://noktashop.istanbul">sex shop</a>
+<a href="https://noktashop.ist">sex shop</a>
+<a href="https://sexshopistanbul.com.tr">sex shop</a>
+<a href="https://noktasexshop.com.tr">sex shop</a>
+<a href="https://seksshopistanbul.net">sex shop</a>
+<a href="https://georgiantransfer.com">Transfer Tbilisi</a>
+<a href="https://georgiantransfer.ge">Transfer Batumi</a>
+<a href="https://batumitransfers.com">Batumi Transfers</a>
+<a href="https://tbilisitransfers.com">Tbilisi Transfers</a>
+<a href="https://flysmsonay.com">sms onay</a>
+<a href="https://sinopotel.com.tr">sinop otelleri</a>
+</div>	
 	
 	
 </body>
